b2c2e36de5bf962301f825de63f998f84c90c4b8
Benjamin Noje <beninoje@hotmail.com>
2025-10-29 20:28:22 -0400

    added correct route to send updated question json

--- cron/fetch-questions.py
+++ cron/fetch-questions.py
@@ -1,19 +1,28 @@
+from anyio import Path
 import requests
 import json
-
-# Raw URL of the JSON file on GitHub
+import os
 url = "https://raw.githubusercontent.com/seanprashad/leetcode-patterns/main/src/data/questions.json"
 
-# Fetch the file
+project_root = Path(__file__).parent.parent
+
+# Path to the data directory
+data_dir = project_root / "src" / "data"
+data_dir.mkdir(parents=True, exist_ok=True)  # make sure the directory exists
+
+# Full path to save the JSON file
+save_path = data_dir / "questions.json"
+    
+
 response = requests.get(url)
 if response.status_code == 200:
     data = response.json()
     print("✅ Fetched JSON successfully")
     
     # Optional: save it locally
-    with open("questions.json", "w") as f:
+    with open(save_path, "w") as f:
         json.dump(data, f, indent=2)
-        print("💾 Saved locally as questions.json")
+        print(f"💾 Saved locally as {save_path}")
 else:
     print(f"❌ Failed to fetch JSON. Status code: {response.status_code}")
 
